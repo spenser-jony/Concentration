5a03d59a197990596cae3889d5fc1915465f673d
Spenser <76523408+spenser-jony@users.noreply.github.com>
2022-06-15 03:26:15 +0300

    2 lec

--- ConcentrationCS193/ViewController.swift
+++ ConcentrationCS193/ViewController.swift
@@ -45,7 +45,7 @@ class ViewController: UIViewController {
     
     func emoji(for card: Card) -> String {
         if emoji[card.identifier] == nil, emojiChoices.count > 0 {
-            let randomIndex = Int(arc4random_uniform(UInt32(emoji.count)))
+            let randomIndex = Int(arc4random_uniform(UInt32(emojiChoices.count)))
             emoji[card.identifier] = emojiChoices.remove(at: randomIndex)
         }
         return emoji[card.identifier] ?? "?"
